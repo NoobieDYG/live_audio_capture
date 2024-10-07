f9975e60ee5a59484399ab5c105b2729e54f8a84
Affaan Jaweed <affaan190@gmail.com>
2024-10-07 15:19:46 +0530

    ready

--- subtitle_display.py
+++ subtitle_display.py
@@ -1,10 +1,10 @@
 import tkinter as tk
-def subtitle_display(subtitle_label):
+root=tk.Tk()
+root.title("Live subtitles")
+root.geometry("800x100")
+subtitle_label = tk.Label(root, text='',font=("Arial",24),wraplength=700)
+subtitle_label.pack()
+root.update()
+root.mainloop()
 
-    root=tk.Tk()
-    root.title("Live subtitles")
-    root.geometry("800x100")
-    subtitle_label = tk.Label(root, text='',font=("Arial",24),wraplength=700)
-    subtitle_label.pack()
-    root.update()
-    root.mainloop()
+#nothing worked
