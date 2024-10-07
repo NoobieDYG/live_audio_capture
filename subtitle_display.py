2d105f3fb201d42d4e81a6c0fd66ea89894b493a
Affaan Jaweed <affaan190@gmail.com>
2024-10-07 14:56:05 +0530

    Trying to use it as function

--- subtitle_display.py
+++ subtitle_display.py
@@ -1,7 +1,10 @@
 import tkinter as tk
-root=tk.Tk()
-root.title("Live subtitles")
-root.geometry("800x100")
-subtitle_label = tk.Label(root, text='',font=("Arial",24),wraplength=700)
-subtitle_label.pack()
-root.mainloop()
+def subtitle_display(subtitle_label):
+
+    root=tk.Tk()
+    root.title("Live subtitles")
+    root.geometry("800x100")
+    subtitle_label = tk.Label(root, text='',font=("Arial",24),wraplength=700)
+    subtitle_label.pack()
+    root.update()
+    root.mainloop()
