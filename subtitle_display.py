21ddd643238180cbde62246b32aba0026946cc21
Affaan Jaweed <affaan190@gmail.com>
2024-10-07 14:42:10 +0530

    Made display window

---
+++ subtitle_display.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+root=tk.Tk()
+root.title("Live subtitles")
+root.geometry("800x100")
+subtitle_label = tk.Label(root, text='',font=("Arial",24),wraplength=700)
+subtitle_label.pack()
+root.mainloop()
