d58b10a5cdd125cb64620cdee1143eb08ee1f16c
Affaan Jaweed <affaan190@gmail.com>
2024-10-07 12:14:18 +0530

    Added audio capturing

---
+++ capture_audio.py
@@ -0,0 +1,26 @@
+import sounddevice as sd
+from scipy.io.wavfile import write
+import numpy as np
+
+#def list_devices():
+    #print(sd.query_devices())
+
+#list_devices()
+
+device_number=3 #got this from list of devices using sounddevice lib
+fs=16000
+duration = 5 #duration of recording
+output_file="chunk.wav" #opened new file to save the recording
+
+
+#saving audio
+def recording_audio_from_livestream():
+    audio = sd.rec(int(duration*fs),samplerate=fs,channels=1,dtype="int16",device=device_number)
+    print("...recording")
+    sd.wait()
+    write(output_file, fs, audio)  #output file , rate , the audio recorded
+    #sd.play(audio,fs)
+    #sd.wait()
+    return audio 
+
+recording_audio_from_livestream() #calling function
