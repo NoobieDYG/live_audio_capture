f88228b764cb0a0969127206185986be1f41c903
Affaan Jaweed <affaan190@gmail.com>
2024-10-07 12:15:32 +0530

    Added speech to text using whisper

---
+++ speech_to_text.py
@@ -0,0 +1,10 @@
+import requests
+import whisper
+
+model = whisper.load_model("small")
+
+audio_path=r"C:\Users\Affaan Jaweed\Desktop\Live audio capture\chunk.wav"
+
+result = model.transcribe(audio_path)
+
+print("Transcription:",result["text"])
