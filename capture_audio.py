fabecc0a61600068a2719f62c63acbf495ed8e7f
Affaan Jaweed <affaan190@gmail.com>
2024-10-08 21:21:39 +0530

    reversed change

--- capture_audio.py
+++ capture_audio.py
@@ -9,8 +9,8 @@ import numpy as np
 
 '''device_number=3 #got this from list of devices using sounddevice lib
 fs=16000
-duration = 1 #duration of recording
-output_file="chunk.wav" #opened new file to save the recording'''
+duration = 1 #duration of recording'''
+output_file="chunk.wav" #opened new file to save the recording
 
 
 #saving audio
@@ -18,7 +18,6 @@ def recording_audio_from_livestream():
     device_number=3 #got this from list of devices using sounddevice lib
     fs=16000
     duration = 1 #duration of recording
-    output_file="chunk.wav" #opened new file to save the recording
 
     audio = sd.rec(int(duration*fs),samplerate=fs,channels=1,dtype="int16",device=device_number)
     print("...recording")
