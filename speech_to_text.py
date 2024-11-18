9a561471c855ce6b46330950d5c34f56e2ef899e
Affaan Jaweed <73639438+NoobieDYG@users.noreply.github.com>
2024-11-18 13:02:40 +0530

    Updated speech_to_text.py

--- speech_to_text.py
+++ speech_to_text.py
@@ -3,7 +3,7 @@ import whisper
 
 model = whisper.load_model("small")
 
-audio_path=r"C:\Users\Affaan Jaweed\Desktop\Live audio capture\chunk.wav"
+audio_path=r" capture\chunk.wav"
 
 result = model.transcribe(audio_path)
 
